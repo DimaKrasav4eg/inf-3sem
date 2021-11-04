85d2d3eeb51bb48ad04b3f8b12f974029b8fee4e
SirDemon <chebotarev.da@phystech.edu>
2021-11-04 21:59:59 +0300

    q

--- 29oct.c
+++ 29oct.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/stat.h>
-
+#include <stdlib.h>
 
 
 int main(int argc, char const *argv[])
@@ -18,38 +18,40 @@ int main(int argc, char const *argv[])
 	stat(argv[1], &buff);
 	int size = buff.st_size;
 	
+
 	if (argc != 3)
 	{
 		printf ("wrong format!\n");
 		return 0;
 	}
 
-	if ((fdin = open (argv[1], O_RDONLY)) = 0 )
+	if ((fdin = open (argv[1], O_RDONLY)) == 0 )
 	{
 		printf ("I can't open %s file\n", argv[1]);
 		return 0;
 	}
-	if ((fdout = open (argv[2], O_RDWR|O_CREAT|O_TRUNC, 0666)) < 0 )
+	if ((fdout = open (argv[2], O_RDWR|O_CREAT|O_TRUNC, 0666)) == 0 )
 	{
 		printf ("I can't open or create %s file\n", argv[2]);
 		return 0;
 	}
 
+	if (lseek (fdout, size - 1, SEEK_SET) == -1)
+	{
+		printf ("lseek error\n");
+		return 0;
+	}
+	write(fdout, "", 1);
 	if ((input = mmap (0, size, PROT_READ, MAP_SHARED, fdin, 0)) == MAP_FAILED)
 	{
 		printf ("mmap fdin error\n");
 		return 0;
 	}
 	
-	if (lseek (fdout, size, SEEK_SET) == -1)
-	{
-		printf ("lseek error\n");
-		return 0;
-	}
 	
-	if ((output = mmap (0, size, PROT_WRITE, MAP_SHARED, fdin, 0)) == MAP_FAILED)
+	if ((output = mmap (0, size, PROT_WRITE, MAP_SHARED, fdout, 0)) == MAP_FAILED)
 	{
-		printf ("mmap fdin error\n");
+		perror("mmap fdout error");
 		return 0;
 	}
 	memcpy(output, input, size);
